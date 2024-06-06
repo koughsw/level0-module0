9aeb86dcf167bf102fcd15d2e60b41e9ada4a8d1
league <yt>
2024-06-06 16:57:04 -0700

    In middle of quiz game

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -9,9 +9,16 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String answer = JOptionPane.showInputDialog("Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
+		if(answer.equals("yes")){
+			JOptionPane.showMessageDialog(null, "you will rule the world");
+			
+		}else{
 
+			JOptionPane.showMessageDialog(null, "sign up for classes at The League");
+		}
+	
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,16 +1,18 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int fizzbuzz = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+		String input = JOptionPane.showInputDialog("What is 9 + 10?");
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if 
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
